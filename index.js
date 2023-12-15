fc25143ddd74c265fec9838a37152a5240dbeb8a
Lotta Tanner <lotta.tanner@helsinki.fi>
2023-12-15 16:05:19 +0200

    uibuild

--- index.js
+++ index.js
@@ -96,5 +96,5 @@ app.use(unknownEndpoint)
   
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`)
+  })
